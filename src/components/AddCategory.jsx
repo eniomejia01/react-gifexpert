70f7b25b9cda127ba10ed05e06370bebed09b368
eniomejia01 <eniomejia57@gmail.com>
2024-01-27 15:28:27 -0600

    first commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+
+export const AddCategory = ( { onNewCategory }) => {
+
+    const [inputValue, setinputValue] = useState('');
+
+    const onInputChange = ( {target}) => { // des-estructuración
+        setinputValue( target.value );
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if( inputValue.trim().length <= 1) return;
+
+        // setCategories( (categories) =>  [ inputValue, ...categories ]);
+        setinputValue('');
+        onNewCategory( inputValue.trim() );
+    }
+
+    return (
+        <form onSubmit={ onSubmit }>
+
+            <input
+                    
+                    type="text"
+                    placeholder="Buscar gifs"
+                    value={inputValue}
+                    onChange={ onInputChange }
+            />
+
+        </form>
+
+    )
+}
